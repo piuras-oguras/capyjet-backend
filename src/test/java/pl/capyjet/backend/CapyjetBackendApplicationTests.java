43e3e6ad0256fbba53b059ff8061dabd67692cd4
piuras-oguras <86265418+piuras-oguras@users.noreply.github.com>
2026-09-17 15:32:16 +0200

    initialize capyjet backend project with Spring Boot and Maven setup

---
+++ src/test/java/pl/capyjet/backend/CapyjetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.capyjet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapyjetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
